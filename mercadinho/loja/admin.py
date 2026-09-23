07e69ff4ec2fd3081a5c25a79d7185ad099227f1
Vitor Daniel <143666152+VitorDanielRC@users.noreply.github.com>
2026-09-23 20:17:46 -0300

    feat:admin

--- mercadinho/loja/admin.py
+++ mercadinho/loja/admin.py
@@ -12,15 +12,14 @@ class VendedorAdmin(admin.ModelAdmin):
 
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ("nome", "vendedor", "categoria", "preco", "estoque", "ativo", "destaque")
-    list_filter = ("categoria", "ativo", "destaque", "vendedor")
-    search_fields = ("nome", "descricao")
-    prepopulated_fields = {"slug": ("nome",)}
+    list_display = ("nome", "vendedor", "preco", "estoque", "ativo")
+    list_filter = ("ativo", "vendedor")
+    search_fields = ("nome", "vendedor__nome")
 
 
 @admin.register(Cupom)
 class CupomAdmin(admin.ModelAdmin):
-    list_display = ("codigo", "desconto_percentual", "valido_ate", "ativo")
+    list_display = ("codigo", "desconto_percentual", "ativo")
     list_filter = ("ativo",)
     search_fields = ("codigo",)
 
@@ -28,27 +27,12 @@ class CupomAdmin(admin.ModelAdmin):
 class ItemPedidoInline(admin.TabularInline):
     model = ItemPedido
     extra = 0
-    can_delete = False
-    readonly_fields = ("produto", "nome_produto", "quantidade", "preco_unitario", "subtotal")
-
-    def has_add_permission(self, request, obj=None):
-        return False
+    readonly_fields = ("produto", "quantidade", "preco_unitario")
 
 
 @admin.register(Pedido)
 class PedidoAdmin(admin.ModelAdmin):
-    list_display = ("__str__", "nome", "criado_em", "total", "status")
-    list_filter = ("status", "criado_em")
-    search_fields = ("nome", "email", "numero")
-    readonly_fields = (
-        "numero", "nome", "email", "telefone", "endereco", "cidade", "cep",
-        "cupom", "subtotal", "desconto", "total", "criado_em",
-    )
-    fields = readonly_fields + ("status",)
-    inlines = [ItemPedidoInline]
-
-    def has_add_permission(self, request):
-        return False
-
-    def has_delete_permission(self, request, obj=None):
-        return False
+    list_display = ("numero", "nome_cliente", "criado_em", "total")
+    search_fields = ("nome_cliente", "numero")
+    readonly_fields = ("numero", "nome_cliente", "criado_em", "cupom", "total")
+    inlines = (ItemPedidoInline,)
